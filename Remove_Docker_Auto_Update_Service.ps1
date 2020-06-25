ed8b64f248dfafc12f50ffd72b926b9b6f05fef7
Damien VAN ROBAEYS <damien.vanrobaeys@gmail.com>
2020-06-25 16:59:03 +0200

    Add files via upload

--- Remove_Docker_Auto_Update_Service.ps1
+++ Remove_Docker_Auto_Update_Service.ps1
@@ -4,4 +4,4 @@ $SystemRoot = $env:SystemRoot
 $Log_File = "$SystemRoot\Debug\Uninstall_Docker_Auto_Update_Service.log"
 $Local_Path_NSSM = "$Docker_Auto_Update_Folder\nssm.exe"
 $Docker_Update_ServiceName = "Docker Desktop Auto Update"
-& $Local_Path_NSSM remove $Docker_Update_ServiceName remove
+& $Local_Path_NSSM remove $Docker_Update_ServiceName confirm
